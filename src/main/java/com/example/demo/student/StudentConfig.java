1b97e7461e1dba9402e007709f55a929f19b5d70
stevenL <stevenl365404@gmail.com>
2021-01-15 16:03:37 +0800

    1. PUT & DELETE

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -13,22 +13,22 @@ public class StudentConfig {
 
     @Bean
     CommandLineRunner commandLineRunner(
-            StudentRepository repository){
+            StudentRepository repository) {
         return args -> {
-            Student mariam =  new Student(
+            Student mariam = new Student(
                     "mariam",
                     "mariam.jamal@gmail.com",
                     LocalDate.of(2000, Month.JANUARY, 5)
             );
 
-            Student alex =  new Student(
+            Student alex = new Student(
                     "alex",
                     "alex@gmail.com",
                     LocalDate.of(2004, Month.FEBRUARY, 5)
             );
 
             repository.saveAll(
-                    List.of(mariam,alex)
+                    List.of(mariam, alex)
             );
         };
     }
